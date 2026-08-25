0f464791c0a1a810b66a8f3d3f36fc41b106b88d
kvichka <39663108+kvichka@users.noreply.github.com>
2026-08-25 07:26:36 +0700

    new with supbase

--- service-worker.js
+++ service-worker.js
@@ -8,7 +8,7 @@
  * BUMP CACHE_NAME ON EVERY PUSH. Skipping it leaves phones on stale files.
  */
 
-const CACHE_NAME = "ento-shell-v14";
+const CACHE_NAME = "ento-shell-v15";
 
 const SHELL = [
   "./",
