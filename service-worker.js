886b18f78f6732c1d00b57b8f22f1460e86c935a
kvichka <39663108+kvichka@users.noreply.github.com>
2026-09-08 14:40:11 +0700

    16

--- service-worker.js
+++ service-worker.js
@@ -8,7 +8,7 @@
  * BUMP CACHE_NAME ON EVERY PUSH. Skipping it leaves phones on stale files.
  */
 
-const CACHE_NAME = "ento-shell-v22";
+const CACHE_NAME = "ento-shell-v23";
 
 const SHELL = [
   "./",
